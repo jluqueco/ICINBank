2619d463a48265eee771117de8a5d0e5ea327806
jluqueco <jluqueco@gmail.com>
2022-09-21 23:50:08 -0400

    dashboard and transactionList components created

--- Front-End/ICINBANK/src/app/app-routing.module.ts
+++ Front-End/ICINBANK/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ErrorComponent } from './error/error.component';
+import { LoginComponent } from './login/login.component';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'dashboard/:username', component: DashboardComponent},
+  {path: 'transactionlist/:account', component: TransactionListComponent},
+  {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Front-End/ICINBANK/src/app/app.module.ts
+++ Front-End/ICINBANK/src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    ErrorComponent,
+    TransactionListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
